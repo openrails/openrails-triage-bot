f308613d4c39ea0dff6e602cf8cfabb28d9d6391
James Ross <git@james-ross.co.uk>
2018-05-23 20:13:34 +0100

    chore: Add configuration management

--- Program.cs
+++ Program.cs
@@ -1,12 +1,43 @@
 ﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
 
 namespace Open_Rails_Triage_Bot
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
-    }
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var config = new CommandLineParser.Arguments.FileArgument('c', "config")
+			{
+				DefaultValue = new FileInfo("config.json")
+			};
+
+			var commandLineParser = new CommandLineParser.CommandLineParser()
+			{
+				Arguments = {
+					config,
+				}
+			};
+
+			try
+			{
+				commandLineParser.ParseCommandLine(args);
+
+				AsyncMain(new ConfigurationBuilder()
+					.AddJsonFile(config.Value.FullName, true)
+					.Build()).Wait();
+			}
+			catch (CommandLineParser.Exceptions.CommandLineException e)
+			{
+				Console.WriteLine(e.Message);
+			}
+		}
+
+		static async Task AsyncMain(IConfigurationRoot config)
+		{
+			Console.WriteLine("Hello World!");
+		}
+	}
 }
